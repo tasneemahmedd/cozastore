079dc69d11b0bbdbdfd0983ea10f9f985d3c1920
tasneem ahmed <ta3549449@gmail.com>
2021-01-16 21:27:11 +0200

    create Navbar

---
+++ plugins/main.js
@@ -0,0 +1,14 @@
+function openside(){
+    document.getElementById("mysidenav").style.width ="400px";
+}
+function closeside(){
+    document.getElementById("mysidenav").style.width ="0";
+}
+
+function openNav() {
+    document.getElementById("mySidenav-menu").style.width = "400px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav-menu").style.width = "0";
+  }
