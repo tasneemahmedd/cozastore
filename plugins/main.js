1970332506a179a714d5a4a39f7f55ec745cda60
Mohamed Ghazi <m.mahmoud@uplift.plus>
2021-01-18 00:23:21 +0200

    Update sidebar

--- plugins/main.js
+++ plugins/main.js
@@ -1,14 +1,14 @@
 function openside(){
-    document.getElementById("mysidenav").style.width ="400px";
+    document.getElementById("mysidenav").style.right ="0";
 }
 function closeside(){
-    document.getElementById("mysidenav").style.width ="0";
+    document.getElementById("mysidenav").style.right ="-400px";
 }
 
 function openNav() {
-    document.getElementById("mySidenav-menu").style.width = "400px";
+    document.getElementById("mySidenav-menu").style.right = "0";
   }
   
   function closeNav() {
-    document.getElementById("mySidenav-menu").style.width = "0";
+    document.getElementById("mySidenav-menu").style.right = "-400px";
   }
