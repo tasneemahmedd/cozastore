6e470e2f2341c18dfc01daa46a25a6b95b54f449
Mohamed Ghazi <m.mahmoud@uplift.plus>
2021-01-28 14:59:51 +0200

    Add carousel overlay & Update Navbar styles

--- plugins/main.js
+++ plugins/main.js
@@ -1,14 +1,15 @@
-function openside(){
-    document.getElementById("mysidenav").style.right ="0";
+function openside() {
+    document.getElementById("mysidenav").style.right = "0";
 }
-function closeside(){
-    document.getElementById("mysidenav").style.right ="-400px";
+
+function closeside() {
+    document.getElementById("mysidenav").style.right = "-400px";
 }
 
 function openNav() {
     document.getElementById("mySidenav-menu").style.right = "0";
-  }
-  
-  function closeNav() {
+}
+
+function closeNav() {
     document.getElementById("mySidenav-menu").style.right = "-400px";
-  }
+}
